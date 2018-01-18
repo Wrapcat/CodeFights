3117e604b217fd6ce0e8b27e52cd466c684adfaf
Kat <KatHiggins05@gmail.com>
2018-01-18 16:57:17 -0500

    Solution

--- centuryfromyear.js
+++ centuryfromyear.js
@@ -1,5 +1,4 @@
-//*Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
-
+/*Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
 Example
 
     For year = 1905, the output should be
@@ -20,9 +19,9 @@ Input/Output
 
     [output] integer
 
-    The number of the century the year is in.*//
+    The number of the century the year is in.*/
     
-//*Solution*//
+//Solution
 
 function centuryFromYear(year) {
 return Math.ceil(year/100);
